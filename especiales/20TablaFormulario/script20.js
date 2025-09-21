d94d3c9201450f9c03f4b16f72652883d681ab3d
Manuel Margulis <margu171005@gmail.com>
2025-09-21 18:22:11 -0300

    parcial

--- especiales/20TablaFormulario/script20.js
+++ especiales/20TablaFormulario/script20.js
@@ -1,40 +1,17 @@
 $(document).ready(function() {
-    var claves = [];
-    $('#cargar').prop('disabled', true);
-    $.getJSON('/Tareas_Redes_Margulis/especiales/padre.json', function(data) {
-        if (!data.ordenesCompra || !data.ordenesCompra.length) return;
-        claves = Object.keys(data.ordenesCompra[0]).filter(function(k){ return k !== 'pdfComprobante'; });
-        var ths = claves.map(function(k){
-            return '<th>' + k.replace(/([A-Z])/g, ' $1').replace(/^./, function(s){ return s.toUpperCase(); }).replace('Id Renglon','ID Renglón').replace('Codigo Producto','Código Producto').replace('Unidad Medida','Unidad Medida').replace('Precio Unitario','Precio Unitario').replace('Importe Renglon','Importe Renglón') + '</th>';
-        }).join('');
-        $('#tablaArticulos thead').html('<tr>' + ths + '</tr>');
-
-        var campos = '';
-        claves.forEach(function(k) {
-            var label = k.replace(/([A-Z])/g, ' $1').replace(/^./, function(s){ return s.toUpperCase(); }).replace('Id Renglon','ID Renglón').replace('Codigo Producto','Código Producto').replace('Unidad Medida','Unidad de Medida').replace('Precio Unitario','Precio Unitario').replace('Importe Renglon','Importe Renglón');
-            if (k === 'unidadMedida') {
-                campos += '<div><label>' + label + ':</label><select name="unidadMedida" id="unidadMedida" required></select></div>';
-            } else if (k === 'fecha') {
-                campos += '<div><label>' + label + ':</label><input type="date" name="' + k + '" required></div>';
-            } else if (k === 'cantidad' || k === 'precioUnitario') {
-                campos += '<div><label>' + label + ':</label><input type="number" step="0.01" name="' + k + '" required></div>';
-            } else if (k === 'importeRenglon') {
-                campos += '<div><label>' + label + ':</label><input type="number" step="0.01" name="' + k + '" id="importeRenglon" readonly></div>';
-            } else {
-                campos += '<div><label>' + label + ':</label><input name="' + k + '" required></div>';
-            }
+    $.getJSON('/Tareas_Redes_Margulis/especiales/hijo.json', function(data) {
+        var $select = $('#unidadMedida');
+        $select.empty();
+        data.unidades.forEach(function(unidad) {
+            $select.append('<option value="' + unidad.codUnidad + '">' + unidad.codUnidad + ' (' + unidad.descripcion + ')</option>');
         });
-        $('.grilla').html(campos);
-        $.getJSON('/Tareas_Redes_Margulis/especiales/hijo.json', function(data) {
-            var $select = $('#unidadMedida');
-            $select.empty();
-            data.unidades.forEach(function(unidad) {
-                $select.append('<option value="' + unidad.codUnidad + '">' + unidad.codUnidad + ' (' + unidad.descripcion + ')</option>');
-            });
-        });
-        $('#cargar').prop('disabled', false);
     });
+
     var filasExtra = [];
+    var claves = [
+        'idRenglon', 'fecha', 'codigoProducto', 'descripcion', 'cantidad', 'unidadMedida', 'precioUnitario', 'importeRenglon'
+    ];
+
     function cargarDatos() {
         $.getJSON('/Tareas_Redes_Margulis/especiales/padre.json', function(data) {
             if (!data.ordenesCompra || !data.ordenesCompra.length) return;
@@ -52,6 +29,7 @@ $(document).ready(function() {
             $('#tablaArticulos tbody').html(filas);
         });
     }
+
     $('#cargar').click(function() {
         cargarDatos();
     });
@@ -70,6 +48,7 @@ $(document).ready(function() {
         $('#modal').hide();
         $('#contenedor').css({'opacity':1,'pointer-events':'auto'});
     });
+
     $(document).on('input', 'input[name="cantidad"], input[name="precioUnitario"]', function() {
         var cantidad = parseFloat($('input[name="cantidad"]').val());
         var precio = parseFloat($('input[name="precioUnitario"]').val());
